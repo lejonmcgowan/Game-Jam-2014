450c3b08e25b3112ed7bcdfb3e192c8d2081894a
kyle-piddington <kpiddy@gmail.com>
2014-01-26 02:36:37 -0800

    Test level 1, Light bounding.
    Lets doo thisssss

--- Assets/Scripts/CamFollowMouseScript.cs
+++ Assets/Scripts/CamFollowMouseScript.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class CamFollowMouseScript : MonoBehaviour {
 	public Transform focusPoint; //Point that can't leave the camera
+
 	public float minFocusRadius;
 	public float minMouseDist = 0.5f;
 	public float cameraRadius = 300;
 	Vector3 attemptedPos;
-	float height = -7;
+	public float height = -10;
 	Vector3 truePos;
 	Vector3 targPos;
 	// Use this for initialization
@@ -25,11 +26,11 @@ public class CamFollowMouseScript : MonoBehaviour {
 		}
 				
 			targPos = attemptedPos - focusPoint.position;				
-			if (targPos.magnitude > cameraRadius + minFocusRadius)
-			{
-				targPos.Normalize ();
-				targPos*=cameraRadius+minFocusRadius;
-			}
+			if (targPos.magnitude > cameraRadius + minFocusRadius) {
+						targPos.Normalize ();
+						targPos *= cameraRadius + minFocusRadius;
+				}
+	
 			/*
 			Debug.Log ("Focus point is:" + focusPoint.position.ToString ());
 			Debug.Log ("Target point is:" + targPos.ToString ());
