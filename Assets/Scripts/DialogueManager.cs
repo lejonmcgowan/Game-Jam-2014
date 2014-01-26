af54d936fc1afb3b8b88aad49dbe8ef00db70157
alling9495 <alex.ling.9495@gmail.com>
2014-01-25 21:19:29 -0800

    GUI, models, and functioning prototype
    GUI scene test run, models for mom, daughter, and a bed.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class DialogueManager : MonoBehaviour {
+
+    /* Class assumes text input is in following format 
+     * "DIALOGUE LINE."
+     * "DIALOGUE LINE 2 ."
+     * */
+    
+    public TextAsset dialogueMom;    
+    public TextAsset dialogueDaughter;
+    public bool test = false;
+    string[] dialogueMomArray;
+    string[] dialogueDaughterArray;
+	// Use this for initialization
+
+    void Start()
+    {
+        initializeDialogue();
+    }
+
+
+    void Update()
+    {
+        if (test == true)
+        {
+            displayDialogue(0, "mom");
+        }
+        else if (test == false)
+        {
+            displayDialogue(0, "daughter");
+        }
+    }
+
+
+    public void initializeDialogue()
+    {
+        dialogueMomArray = dialogueMom.text.Split('.');
+        dialogueDaughterArray = dialogueDaughter.text.Split('.');
+    }
+
+
+    public void displayDialogue(int dialogueLineNumber, string character)
+    {
+       TextMesh display = (TextMesh)GetComponent("TextMesh");
+        
+        if (character == "mom")
+        {
+            display.text = dialogueMomArray[dialogueLineNumber];
+        }
+        else if (character == "daughter")
+        {
+            display.text = dialogueDaughterArray[dialogueLineNumber];
+        }
+        
+    }
+
+
+
+}
